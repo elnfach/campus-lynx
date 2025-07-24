c8defe23038edb4de36e5625bddcc52178cedbca
elnfach <105500168+elnfach@users.noreply.github.com>
2025-07-24 20:49:18 +0800

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,16 +1,15 @@
 import {createContext, useContext} from 'react';
+import type {AppTheme} from "@/components/theme/interface/appTheme.ts";
 
 type ThemeContextType = {
-    isDarkMode: boolean;
+    theme: AppTheme;
+    isDark: boolean;
     toggleTheme: () => void;
 };
-
 export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const useTheme = () => {
     const context = useContext(ThemeContext);
-    if (context === undefined) {
-        throw new Error('themeContext must be used within a ThemeProvider');
-    }
+    if (!context) throw new Error("useTheme must be used within a ThemeProvider");
     return context;
 };
