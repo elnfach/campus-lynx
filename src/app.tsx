510599c99341e2f68299652c146d521f95a179bf
elnfach <105500168+elnfach@users.noreply.github.com>
2025-07-24 11:34:28 +0800

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -5,10 +5,11 @@ import About from "@/pages/about/about";
 import NotFound from "@/pages/404/404";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import {ThemeProvider} from "@/components/themeProvider.tsx";
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
         <Header/>
         <Router>
             <Routes>
@@ -16,12 +17,16 @@ function App() {
                     path="/"
                     element={<Home/>}
                 />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<NotFound />} />
+                <Route
+                    path="/about"
+                    element={<About/>}/>
+                <Route
+                    path="*"
+                    element={<NotFound/>}/>
             </Routes>
         </Router>
         <Footer/>
-    </>
+    </ThemeProvider>
   )
 }
 
