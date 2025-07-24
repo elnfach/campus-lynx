71b94382ce94ba5ee4901bd268a45d274e933a92
elnfach <105500168+elnfach@users.noreply.github.com>
2025-07-24 11:34:12 +0800

    Update useStorageUrl.ts

--- src/hooks/useStorageUrl.ts
+++ src/hooks/useStorageUrl.ts
@@ -6,7 +6,18 @@ export const useStorageUrl = (path: string) => {
     const [url, setUrl] = useState("");
 
     useEffect(() => {
-        getDownloadURL(ref(storage, path)).then(setUrl);
+        const cachedUrl = localStorage.getItem(`firebase:${path}`);
+        const cachedTime = localStorage.getItem(`firebase:${path}:time`);
+
+        if (cachedUrl && cachedTime && Date.now() - Number(cachedTime) < 86400000) {
+            setUrl(cachedUrl);
+        } else {
+            getDownloadURL(ref(storage, path)).then((url) => {
+                setUrl(url);
+                localStorage.setItem(`firebase:${path}`, url);
+                localStorage.setItem(`firebase:${path}:time`, Date.now().toString());
+            });
+        }
     }, [path]);
 
     return url;
