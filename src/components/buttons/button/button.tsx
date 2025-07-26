639aa9472d21dae15f4b6d61ec647cf1770c0ba0
elnfach <105500168+elnfach@users.noreply.github.com>
2025-07-26 21:25:13 +0800

    Create button.tsx

---
+++ src/components/buttons/button/button.tsx
@@ -0,0 +1,38 @@
+import type {ButtonProps} from "@/components/buttons/interface/buttonProps.ts";
+import React from "react";
+import {useTheme} from "@/hooks/useTheme.ts";
+
+const Button = (
+    {
+        className = "",
+        disabled = false,
+        fullWidth = false,
+        text,
+        ...rest
+    }: ButtonProps
+) => {
+    const { theme } = useTheme();
+
+    const baseClasses = 'hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-md transition duration-200';
+    const buttonClasses = `
+    ${baseClasses}
+    ${theme.colors.primary} 
+    ${theme.colors.onPrimary} 
+    ${theme.shapes.medium}  
+    ${fullWidth ? 'w-full' : ''}
+    ${disabled ? "opacity-50 cursor-not-allowed" : ""}
+    ${className}
+    `;
+
+    return (
+        <button
+            className={buttonClasses}
+            disabled={disabled}
+            {...rest}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button;
