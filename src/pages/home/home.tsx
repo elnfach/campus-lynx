2821b21c3cd504944f830f7738c6af8a11295b1e
elnfach <105500168+elnfach@users.noreply.github.com>
2025-08-11 03:53:53 +0800

    Update home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,35 @@
 import '@styles/home/home.css'
 import Articles from "@/components/articles"
 import InvitationForm from "@/components/invitationForm"
-//import {DarkModeToggle} from "../../components/darkModeToggle.tsx";
+import Present from "@/components/present.tsx";
+import {useTheme} from "@/hooks/useTheme.ts";
+import {Accordion} from "@/components/accordion.tsx";
+import H3 from "@/components/text/h3.tsx";
+import EducationalForm from "@/components/educationalForm.tsx";
+import EducationalProgramme from "@/components/educationalProgramme.tsx";
+import {faqItems} from "@/config/common.ts";
 
 function Home() {
+    const {theme} = useTheme();
+
+    const baseClasses = 'w-full h-full mx-auto px-6 py-12';
+    const homeClasses = `
+    ${baseClasses}
+    ${theme.colors.background}
+    `;
+
     return (
-        <div className="w-full h-full bg-gray-50">
-            <main className="container mx-auto px-6 py-12">
-                <InvitationForm/>
-                <Articles/>
-            </main>
-        </div>
+        <main className={homeClasses}>
+            <Present/>
+            <InvitationForm/>
+            <H3 className="text-center text-university-primary m-8">Часто задаваемые вопросы</H3>
+            <Accordion items={faqItems}/>
+            <EducationalForm/>
+            <div id="college">
+                <EducationalProgramme/>
+            </div>
+            <Articles/>
+        </main>
     );
 }
 
